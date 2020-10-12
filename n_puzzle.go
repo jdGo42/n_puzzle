b8d7e63776d746e08c4be42feb060492f7dbe272
jdgo42 <jdgo42@mail.ru>
2020-10-12 03:06:40 +0200

    add maps, beginning of main and one usefull link

---
+++ n_puzzle.go
@@ -0,0 +1,17 @@
+package npuzzle
+
+import (
+//	"os" // for open and read files
+//	"fmt" // for print
+)
+
+func main() {
+	/*
+	handle args
+	handle options (put into global env)
+	check if file is well formated
+	check if file is solvable
+	solve and display solutions
+	return ;-)
+	*/
+}
