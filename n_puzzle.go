55a933ecd16b903f01c31250b087ff9ddae8b818
jdgo42 <jdgo42@mail.ru>
2020-10-12 04:48:20 +0200

    add todolist for checkformat

--- n_puzzle.go
+++ n_puzzle.go
@@ -6,6 +6,21 @@ import (
 	"os"	// for Args and exit
 )
 
+func checkFormat(data []byte) bool {
+	fmt.Printf("string read:\n%s\n", data)
+	/*
+	split by \n
+	check we only need numbers # \t spaces and \n
+	kick lines beginning with #
+	forget string between # and \n
+	check if we have a line with size at the very beginning
+	check if we only have numbers  before \n or #, and we should have x numbers by line and x lines of numbers (x = size)
+	if all those lines are good we have a good format, we can try to solve this puzzle
+	*/
+
+	return true
+}
+
 func main() {
 
 	if 2 == len(os.Args) {
@@ -14,7 +29,7 @@ func main() {
      		fmt.Print("\n\033[1;31mNo such file or directory\033[m\n")
      		os.Exit(-1)
     	}
-    	fmt.Printf("string read:%s\n", data)
+    	checkFormat(data) // cast a []byte into string, not 
     } else {
     	fmt.Printf("\n\033[1;31mPlease put only one file in argument, currently, there is %d argument(s)\033[m\n", len(os.Args) - 1 )
     }
