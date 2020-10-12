825d4b09e03ecbeb74e1026d09c77eb88d2d1a4f
jdgo42 <jdgo42@mail.ru>
2020-10-12 03:56:21 +0200

    first parsing of args, just check if file exists

--- n_puzzle.go
+++ n_puzzle.go
@@ -1,11 +1,24 @@
-package npuzzle
+package main
 
 import (
-//	"os" // for open and read files
-//	"fmt" // for print
+	"io/ioutil"	// for open and read files
+	"fmt"	// for print
+	"os"	// for Args and exit
 )
 
 func main() {
+
+	if 2 == len(os.Args) {
+    	data, err := ioutil.ReadFile(os.Args[1])
+    	if err != nil {
+     		fmt.Print("\n\033[1;31mNo such file or directory\033[m\n")
+     		os.Exit(-1)
+    	}
+    	fmt.Printf("string read:%s\n", data)
+    } else {
+    	fmt.Printf("\n\033[1;31mPlease put only one file in argument, currently, there is %d argument(s)\033[m\n", len(os.Args) - 1 )
+    }
+    return
 	/*
 	handle args
 	handle options (put into global env)
